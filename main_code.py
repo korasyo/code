9c5dab9307153ad6782f3c8059a8dce42986d619
korasyo <s20C1127UP@s.chibakoudai.jp>
2024-01-16 15:02:19 +0900

    main

---
+++ main_code.py
@@ -0,0 +1,38 @@
+import spidev
+from time import sleep
+
+V_REF = 3.29476
+CHN = 0
+
+spi = spidev.SpiDev()
+spi.open(0, 0)
+spi.max_speed_hz = 1000000
+
+def get_voltage():
+    dout = spi.xfer2([((0b1000+CHN)>>2)+0b100,((0b1000+CHN)&0b0011)<<6,0])
+    bit12 = ((dout[1]&0b1111) << 8) + dout[2]
+    volts = round((bit12 * V_REF) / float(255),4)
+    return volts
+
+def get_voltages():
+    douts = spi.xfer2([((0b1000+CHN)>>2)+0b100,((0b1000+CHN)&0b0011)<<6,0])
+    bits12 = ((douts[1]&0b1111) << 8) + douts[2]
+    volt = round((bits12 * V_REF) / float(255),4)
+    print(volt)
+get_voltages()
+
+input ('please push enter')
+
+number = get_voltage()
+
+try:
+    print('--- start program ---')
+    while True:
+        volts = get_voltage() - float(number)
+        print('volts= {:3.2f}'.format(volts))
+        sleep(0.01)
+except KeyboardInterrupt:
+        pass
+finally:
+    spi.close()
+    print('--- stop program ---')
